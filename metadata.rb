129fc442a2154f55d53dbce7287ea9a5dc3af223
Vyacheslav Voronenko <git@voronenko.info>
2016-12-04 23:21:00 +0200

    improve(centos) fixed play for centos

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'git@voronenko.info'
 license 'MIT'
 description 'Installs/Configures pip, virtualenv, virtualenvwrapper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.2'
+version '0.0.3'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
